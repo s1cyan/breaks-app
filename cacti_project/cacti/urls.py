18a45b40e502c1ce3a56bd6247dbc0b90600131f
Porrith Suong <psuong95@gmail.com>
2016-06-05 02:00:49 -0400

    Adds the layout and template for the user registration page.

--- cacti_project/cacti/urls.py
+++ cacti_project/cacti/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.render_home_page, name="home")
+    url(r'^$', views.render_home_page, name="home"),
+    url(r'^user-registration', views.render_user_registration, name="user-registration")
 ]
--- cacti_project/cacti/views.py
+++ cacti_project/cacti/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def render_home_page(request):
     return render(request, "default.html")
+
+
+def render_user_registration(request):
+    return render(request, "user_registration.html")
